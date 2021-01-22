d96b88f5c8cafa6283457ee85ae206905d804d37
huntersudo <huntersudo@126.com>
2021-01-22 14:21:56 +0800

    20210122

--- gopl_io/ch1/fetch/main.go
+++ gopl_io/ch1/fetch/main.go
@@ -16,22 +16,24 @@ import (
 
 func main() {
 	//for _, url := range os.Args[1:] {
-	url :="http://gopl.io"
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		//b, err := ioutil.ReadAll(resp.Body)
+	url := "http://gopl.io"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	//b, err := ioutil.ReadAll(resp.Body)
 	_, err = ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		//fmt.Printf("%s", b)
-		fmt.Printf("%s", resp.Status)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	//fmt.Printf("%s", b)
+	fmt.Printf("%s", resp.Status)
 	//}
+
+	//test
 }
 
 //!-
